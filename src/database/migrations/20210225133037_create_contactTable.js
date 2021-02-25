6cccd268271dd9ea6827b7c20b060da1b6f35b3e
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-02-25 13:44:47 -0300

    Create location migration

---
+++ src/database/migrations/20210225133037_create_contactTable.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('contacts',(table)=>{
+      table.increments('contact_id').primary();
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('phone').notNullable();
+      table.string('loc_id').notNullable();
+      table.foreign('loc_id').references('locations.loc_id');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.droptTable('contacts');
+};
